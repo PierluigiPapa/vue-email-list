e3bc2e3744ef39e34308f3c14189633fc1937db5
PierluigiPapa <pierluigipapa9@gmail.com>
2024-02-05 16:46:17 +0100

    feat: add list email generated by api

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,12 +1,20 @@
-createApp ({
-    data() {
+const {createApp} = Vue;
+
+createApp({
+    data(){
         return {
             apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
-             
+            cpuGeneratorMail : null,
+            emailList: [],
+        };
+    },
+    methods: {},
+    mounted() {
+        for(let i = 0; i < 10; i++) {
+            axios.get(this.apiUrl).then((data) => {
+                const result = data.data.response;
+                this.emailList.push(result);
+                });
+            }
         }
-    },
-    created() {
-    },
-    methods : {
-    }
-}).mount ('#app')
+    }).mount('#app');
